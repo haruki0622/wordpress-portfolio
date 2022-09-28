08afc9026974a6a695bcf3dba5780f907271ee2f
Haruki Izumihara <izu.0622h@gmail.com>
2022-09-28 12:54:38 +0900

    added wp_head

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="<?php echo get_theme_file_uri(); ?>/css/style.css?ver=0.6" />
+  <link rel="stylesheet" href="<?php echo get_theme_file_uri(); ?>/css/style.css?ver=1.0" />
   <title>泉原遥輝 | Haruki Izumihara</title>
-  <!-- <?php wp_head(); ?> -->
+  <?php wp_head(); ?>
 </head>
 <body>
   <div class="Container">
